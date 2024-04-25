7ca210b2899f94ccc0a094e1840ae25267961c0b
MgMendoza-23 <miguelgommendoza@gmail.com>
2024-04-24 18:09:10 -0600

    Coreccion del error

--- routes/Auth.js
+++ routes/Auth.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import AuthController from '../controllers/Auth.js';
-
+const AuthController = require('../controllers/Auth.js');
 const api = express.Router();
 
 api.post("/registro", AuthController.Registrar);
