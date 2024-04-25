33437441e30a3a0bdcc5e332b06fd4ba2d52c787
MgMendoza-23 <miguelgommendoza@gmail.com>
2024-04-24 18:05:51 -0600

    Coreccion del error

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ import DatosR from "../routes/Datos.routes.js";
 import AuthRoutes from './Auth.js'
 
 const router = routerx();
+const AuthController = require('../controllers/Auth.js');
 
 router.use("/datos", DatosR);
 router.use('/auth', AuthRoutes);
